76668d9c033239ec8ac1692bd8f81e0840739bca
Daniel Taccola <danieltaccola@yahoo.com.br>
2022-06-23 23:01:12 -0300

    autenticação e criação de subscriber

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4om8u4w0ctu01yw95xmdbqw/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
